27bcc742afd867b14fe00237af414750edeccba6
Dileo Moreira <dileo.moreira@tecnogroup.com.br>
2020-04-01 22:13:58 -0300

    commit inicial

---
+++ src/main/java/br/com/dem/bota/BotaApplication.java
@@ -0,0 +1,13 @@
+package br.com.dem.bota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dem/bota/BotaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dem.bota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
